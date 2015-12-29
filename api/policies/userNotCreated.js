c15d6759d7041334e77b4767295163696502d785
unknown <2000haiming@163.com>
2015-12-29 22:25:07 +0800

    update

--- api/policies/userNotCreated.js
+++ api/policies/userNotCreated.js
@@ -1,11 +1,11 @@
 module.exports = function (req, res, next) {
     // 检查数据库中是否已经有用户
-    User.find().exec(function(err,users){
-    	sails.log.info(users);
-        if(users.length){
-            res.redirect('/logout');
-        }else {
-            next();
-        }
-    });
+    // User.find().exec(function(err,users){
+    // 	sails.log.info(users);
+    //     if(users.length){
+    //         res.redirect('/logout');
+    //     }else {
+    //         next();
+    //     }
+    // });
 };
