3e0c36621c57358e8ed56d48d0e7d9e5838b1aaa
unknown <2000haiming@163.com>
2015-12-30 15:29:21 +0800

    a

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -12,11 +12,12 @@
 module.exports.bootstrap = function(cb) {
 	// 启动时刷新站点信息
     User.find().exec(function(err,users){
-        if(users.length > 0){
-            var user = users[0];
-            sails.config.user.name = user.username;
-            sails.config.user.right = user.right;
-        }
+        if(users)
+            if(users.length > 0){
+                var user = users[0];
+                sails.config.user.name = user.username;
+                sails.config.user.right = user.right;
+            }
     });
     //刷新学生信息
     Stu.find().exec(function(err, found){
